1a54d480d97e414a8698ae738d8ebd75f4c7b106
Jorge Tordoya <jtordoya.bo@gmail.com>
2024-10-28 17:16:56 -0400

    TREBOL-18

--- src/app/main/modal-import-report/modal-import-report.component.ts
+++ src/app/main/modal-import-report/modal-import-report.component.ts
@@ -1,10 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { RoutesService } from '../../api/routes.service';
 import { PointsService } from '../../api/points.service';
-import { DistrictsService } from '../../api/districts.service';
 import { DistrictPointsService } from '../../api/districtspoints.service';
-import { PersonalService } from '../../api/personal.service';
-import { PersonsService } from '../../apipersonal.service.ts/persons.services';
 import { JobroutesService } from '../../apipersonal.service.ts/jobroutes.services';
 
 import olMapScreenshot from 'ol-map-screenshot';
@@ -13,10 +10,6 @@ import * as olSphere from 'ol/sphere';
 
 import { jsPDF } from "jspdf";
 import { MapComponent } from 'ng-openlayers';
-//declare var jsPDF: any;
-declare var $:any;
-//const { jsPDF } = window.jspdf;
-
 @Component({
   selector: 'app-modal-report',
   templateUrl: './modal-import-report.component.html',
@@ -111,34 +104,25 @@ export class ModalImportReportComponent implements OnInit {
 	closeModal(){
 		this.onClose.emit();
 	}
-	printPdf(){		
-		/*const doc = new jsPDF({
-			orientation: "landscape",
-			unit: "px",
-			//format: [4, 2]
-		  });*/
-		  
+	printPdf(){				  
 		const doc  = new jsPDF('l', 'px', 'letter');
-		//doc.setFont('PTSans'); 
-		doc.setFontSize(10); 
 		
-		  
-			doc.addImage(this.thumbb64Route, 'jpeg', 20, 20,  270, 190);
-			doc.addImage(this.thumbb64Check, 'jpeg', 300, 20,  270, 190);
-			doc.addImage(this.thumbb64Track, 'jpeg', 20, 230,  270, 190);
+		doc.addImage(this.thumbb64Route, 'jpeg', 20, 20,  270, 190);
+		doc.addImage(this.thumbb64Check, 'jpeg', 300, 20,  270, 190);
+		doc.addImage(this.thumbb64Track, 'jpeg', 20, 230,  270, 190);
 
-			doc.text("Ruta designada", 20, 15);  
-			doc.text("Ruta completada", 320, 15);
-			doc.text("Trayecto recorrido ", 20, 225);  
+		doc.setFontSize(10);
 
+		doc.text("Ruta designada", 20, 15);  
+		doc.text("Ruta completada", 320, 15);
+		doc.text("Trayecto recorrido ", 20, 225);  
 
-			doc.setFontSize(12); 
-			
-			doc.text("Nombre: " + this.device.personal.name, 350, 285);
-			doc.text("Ruta: " + this.device.routeSelected.name, 350, 295);
-			doc.text("Completado: " + this.device.states['COMPLETED'] + '%', 350, 305);  
-			//doc.text("Hello world!", 10, 10);  
-			doc.save("report.pdf"); 
+		doc.setFontSize(12); 
+		
+		doc.text("Nombre: " + this.device.personal.name, 350, 285);
+		doc.text("Ruta: " + this.device.routeSelected.name, 350, 295);
+		doc.text("Completado: " + this.device.states['COMPLETED'] + '%', 350, 305);  
+		doc.save("report.pdf"); 
 	}
 	centerMap(){
 		console.log("this.device.routeSelected",this.device.routeSelected);
@@ -147,21 +131,6 @@ export class ModalImportReportComponent implements OnInit {
 		const corner2 = transform([extent[2],extent[3]], 'EPSG:4326', 'EPSG:3857');
 		const extent3857 = [corner1[0],corner1[1],corner2[0],corner2[1]];
 		console.log("extent3857",extent3857);
-	/*	this.mapRoute.instance.getView().fit(extent3857, {
-			maxZoom: 18,
-			padding:[50,50,50,50],
-			duration: 300
-		});	
-		this.mapCheck.instance.getView().fit(extent3857, {
-			maxZoom: 18,
-			padding:[50,50,50,50],
-			duration: 300
-		});	
-		this.mapTrack.instance.getView().fit(extent3857, {
-			maxZoom: 18,
-			padding:[50,50,50,50],
-			duration: 300
-		});*/	
 		this.mapPrint.instance.getView().fit(extent3857, {
 			maxZoom: 18,
 			padding:[50,50,50,50],
